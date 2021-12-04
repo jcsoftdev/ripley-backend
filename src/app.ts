b94a7e3584ee31183453515c80c19a1eb2d2dca3
Juan Carlos <jcvalencialopez@proveedor.teco.com.ar>
2021-12-04 17:10:30 -0500

    fix formdata

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import * as dotenv from "dotenv"
 import express from 'express'
-import formData from 'express-form-data'
 import cors from 'cors';
 import userRoute from './routes/user'
 
 dotenv.config({ path: __dirname+'/.env' })
 
 const app = express()
-
+app.use(express.json())
 //middleWare
 // app.use(morgan('dev'))
-app.use(formData.parse())
+// app.use(formData.parse())
+
 app.use(cors());
 
 //routes
